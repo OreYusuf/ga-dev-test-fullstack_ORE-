db1b60b1d1188e0ce93af12b0ef7440952058455
OreYusuf <52608450+OreYusuf@users.noreply.github.com>
2021-02-04 00:52:55 +0000

    Initial Commit

--- api/routes.js
+++ api/routes.js
@@ -7,16 +7,11 @@ router.param('lrPropertyId', async (id, ctx, next) =>
 {
 	ctx.lrProperty = await new lrProperty({id: id}).fetch({withRelated: ['lrTransactions'], require: false});
 
-	if(! ctx.lrProperty){
-
-		ctx.lrProperty = await new lrProperty({street: street}).fetch({withRelated: ['lrTransactions'], require: false});
-
-		}else{
-
-			ctx.status = 404;
-			return ctx.body = {error: true, msg: "LRProperty not found"};
-		}
-	
+	if(! ctx.lrProperty)
+	{
+		ctx.status = 404;
+		return ctx.body = {error: true, msg: "LRProperty not found"};
+	}
 
 	return next();
 
@@ -28,6 +23,25 @@ router.param('lrPropertyId', async (id, ctx, next) =>
 .get('/lrProperty/:lrPropertyId', async (ctx, next) => 
 {
 	return ctx.body = {success: true, lrProperty: ctx.lrProperty.toJSON()};
+});
+
+
+router.param('lrPropertyStreet', async (id, ctx, next) =>
+{
+	ctx.lrProperty = await new lrProperty({street: street}).fetch({withRelated: ['lrTransactions'], require: false});
+
+	if(! ctx.lrProperty)
+	{
+		ctx.status = 404;
+		return ctx.body = {error: true, msg: "LRProperty not found"};
+	}
+
+	return next();
+
+})
+.get('/', async (ctx, next) => 
+{
+	return ctx.body = "I'm alive!";
 })
 .get('/lrProperty/:street', async (ctx, next) => 
 {
